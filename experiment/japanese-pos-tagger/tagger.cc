a741d907db1c59b1a496f38a250abdf5397310c2
Syoyo Fujita <syoyo@lighttransport.com>
2024-10-13 05:23:05 +0900

    w.i.p.

--- experiment/japanese-pos-tagger/tagger.cc
+++ experiment/japanese-pos-tagger/tagger.cc
@@ -1,5 +1,24 @@
 #include <unordered_map>
 
+static bool longestPatternSearch(const char *key, uint32_t key_size, int prev_pos_id)
+{
+
+  // traverse each char
+  
+
+  // 
+  // If prev POS id is provided, consider it in the traverseal,
+  // 0 = BOS(= no prev POS id)
+  // 
+  if (!prev_pos_id) {
+    return n;
+  }
+
+
+}
+
 int main(int argc, char **argv) {
+
+  // concat 
   return 0;
 }
